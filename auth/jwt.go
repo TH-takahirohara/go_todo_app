e8b997f05a7162d8b516abc583d526df56bd3ba9
Takahiro Hara <takahiro.hb@gmail.com>
2022-11-26 16:55:28 +0900

    add GenerateToken method

--- auth/jwt.go
+++ auth/jwt.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"time"
 
 	"github.com/TH-takahirohara/go_todo_app/clock"
 	"github.com/TH-takahirohara/go_todo_app/entity"
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v2/jwa"
 	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 )
 
 //go:embed cert/secret.pem
@@ -50,4 +54,33 @@ func parse(rawKey []byte) (jwk.Key, error) {
 		return nil, err
 	}
 	return key, nil
+}
+
+const (
+	RoleKey     = "role"
+	UserNameKey = "user_name"
+)
+
+func (j *JWTer) GenerateToken(ctx context.Context, u entity.User) ([]byte, error) {
+	tok, err := jwt.NewBuilder().
+	  JwtID(uuid.New().String()).
+		Issuer(`github.com/TH-takahirohara/to_todo_app`).
+		Subject("access_token").
+		IssuedAt(j.Clocker.Now()).
+		Expiration(j.Clocker.Now().Add(30*time.Minute)).
+		Claim(RoleKey, u.Role).
+		Claim(UserNameKey, u.Name).
+		Build()
+	if err != nil {
+		return nil, fmt.Errorf("GetToken: failed to build token: %w", err)
+	}
+	if err := j.Store.Save(ctx, tok.JwtID(), u.ID); err != nil {
+		return nil, err
+	}
+
+	signed, err := jwt.Sign(tok, jwt.WithKey(jwa.RS256, j.PrivateKey))
+	if err != nil {
+		return nil, err
+	}
+	return signed, nil
 }
