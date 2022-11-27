bef9f3bf4c0a2101b452b979b5ec0981f50da863
Takahiro Hara <takahiro.hb@gmail.com>
2022-11-27 20:31:43 +0900

    update task (p.256 - p.261)

--- store/task.go
+++ store/task.go
@@ -11,9 +11,9 @@ func (r *Repository) AddTask(
 ) error {
 	t.Created = r.Clocker.Now()
 	t.Modified = r.Clocker.Now()
-	sql := `INSERT INTO task (title, status, created, modified) VALUES (?, ?, ?, ?)`
+	sql := `INSERT INTO task (user_id, title, status, created, modified) VALUES (?, ?, ?, ?, ?)`
 	result, err := db.ExecContext(
-		ctx, sql, t.Title, t.Status,
+		ctx, sql, t.UserID, t.Title, t.Status,
 		t.Created, t.Modified,
 	)
 	if err != nil {
@@ -28,11 +28,11 @@ func (r *Repository) AddTask(
 }
 
 func (r *Repository) ListTasks(
-	ctx context.Context, db Queryer,
+	ctx context.Context, db Queryer, id entity.UserID,
 ) (entity.Tasks, error) {
 	tasks := entity.Tasks{}
-	sql := `SELECT id, title, status, created, modified FROM task;`
-	if err := db.SelectContext(ctx, &tasks, sql); err != nil {
+	sql := `SELECT id, user_id, title, status, created, modified FROM task WHERE user_id = ?;`
+	if err := db.SelectContext(ctx, &tasks, sql, id); err != nil {
 		return nil, err
 	}
 	return tasks, nil
